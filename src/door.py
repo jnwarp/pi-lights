e6b0428608a1b1d33d0060a8bd633208571435c8
James Warp <james@jnwarp.com>
2018-10-26 15:54:06 -0400

    Update door to control motion sensor

--- src/door.py
+++ src/door.py
@@ -1,14 +1,15 @@
 import RPi.GPIO as GPIO  
 from time import sleep     # this lets us have a time delay (see line 15)  
 GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering  
-GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)    # set GPIO25 as input (button)  
-GPIO.setup(21, GPIO.OUT)   # set GPIO24 as an output (LED)  
-GPIO.output(21, True)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)    # set GPIO25 as input (button)  
+GPIO.setup(27, GPIO.OUT)   # set GPIO24 as an output (LED)  
+GPIO.output(27, True)
   
 try:  
     while True:            # this will carry on until you hit CTRL+C  
-        print(GPIO.input(20))
+        print(GPIO.input(4))
         sleep(0.1)         # wait 0.1 seconds  
+        GPIO.output(27, GPIO.input(4))
   
 finally:                   # this block will run no matter how the try block exits  
     GPIO.cleanup()         # clean up after yourself 
