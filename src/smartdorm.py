c2941ee8bfa05b90f651a5d6a8ea02ba89b4143b
Michael Klaassen <mjk6017@gmail.com>
2018-11-29 12:25:57 -0500

    Fix #4

--- src/smartdorm.py
+++ src/smartdorm.py
@@ -1,30 +1,12 @@
 import argparse
 import lights
 
+
 def lights():
 
-    strip1 = LightStrip(rgb1)
-    strip2 = LightStrip(rgb2)
-def setColor(self, color = 'white', brightness = 255):
-    # get the rgb color value ex (255, 255, 0)
-    if type(color) == str:
-        rgb = self.colors[color]
-    else:
-        rgb = color
+    strip1 = lights.LightStrip(rgb1)
+    strip2 = lights.LightStrip(rgb2)
 
-    for i in range(3):
-        # skip unset pins
-        if self.pins[i] < 1:
-            continue
-
-        # set the color for each pin
-        level = int(rgb[i] * (brightness / 255))
-        self.pi.set_PWM_dutycycle(self.pins[i], level)
-
-    # save the current color rgbNow = list(rgb) rgbNow.append(brightness)
-    rgb = list(rgb)
-    rgb.append(brightness)
-    self.currentColor = tuple(rgb)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Choose the Pi')
@@ -39,8 +21,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if (args.piPlatform == 'lights'):
-        strip1 = LightStrip(rgb1)
-        strip2 = LightStrip(rgb2)
+        strip1 = lights.LightStrip(rgb1)
+        strip2 = lights.LightStrip(rgb2)
         while True:
             strip1.setColor('orange')
             strip2.setColor('orange')
