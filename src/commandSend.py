e9d86cbe399ccf3c7e6bd18a1bbd195a91312f2e
James Warp <james@jnwarp.com>
2018-11-29 15:43:59 -0500

    Convert spaces to tabs

--- src/commandSend.py
+++ src/commandSend.py
@@ -2,20 +2,20 @@ from requests import Session
 from requests_toolbelt import MultipartEncoder
 
 class CommandSend():
-    def __init__(self, token, url='https://localhost/', verify=False):
+	def __init__(self, token, url='https://localhost/', verify=False):
 		self.url = url
 		self.verify = verify
 		self.session = Session()
-        self.token = token
+		self.token = token
 
 		# get cookie headers
 		self.session.head(self.url, verify=self.verify)
 
-    def send(self, command, data={}):
-        response = self.session.post(
+	def send(self, command, data={}):
+		response = self.session.post(
 			url = self.url + 'commandReceive',
 			params = {
-                'token': self.token,
+				'token': self.token,
 				'command': command
 			},
 			data = data,
@@ -23,5 +23,5 @@ class CommandSend():
 			verify = self.verify
 		)
 
-        print(response.text)
-        return response
+		print(response.text)
+		return response
--- src/smartdorm.py
+++ src/smartdorm.py
@@ -3,41 +3,41 @@ import lights
 import time
 
 def runLights():
-    rgb1 = (20, 16, 21)
-    rgb2 = (13, 26, 19)
+	rgb1 = (20, 16, 21)
+	rgb2 = (13, 26, 19)
 
-    strip1 = lights.LightStrip(rgb1)
-    strip2 = lights.LightStrip(rgb2)
+	strip1 = lights.LightStrip(rgb1)
+	strip2 = lights.LightStrip(rgb2)
 
-    strip1.fadeColor('red')
-    time.sleep(1)
-    strip1.fadeColor('green')
-    time.sleep(1)
-    strip1.fadeColor('blue')
+	strip1.fadeColor('red')
+	time.sleep(1)
+	strip1.fadeColor('green')
+	time.sleep(1)
+	strip1.fadeColor('blue')
 
-    strip2.fadeColor('red')
-    time.sleep(1)
-    strip2.fadeColor('green')
-    time.sleep(1)
-    strip2.fadeColor('blue')
+	strip2.fadeColor('red')
+	time.sleep(1)
+	strip2.fadeColor('green')
+	time.sleep(1)
+	strip2.fadeColor('blue')
 
 def runDoor():
-    cp = ControlPanel()
+	cp = ControlPanel()
 	cp.startServer()
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Choose the Pi')
+	parser = argparse.ArgumentParser(description='Choose the Pi')
 
-    parser.add_argument('--door', dest='piPlatform', action='store_const',
-        const='door',
-        help='runs the "door" Raspberry Pi')
-    parser.add_argument('--lights', dest='piPlatform', action='store_const',
-        const='lights',
-        help='runs the "lights" Raspberry Pi')
+	parser.add_argument('--door', dest='piPlatform', action='store_const',
+		const='door',
+		help='runs the "door" Raspberry Pi')
+	parser.add_argument('--lights', dest='piPlatform', action='store_const',
+		const='lights',
+		help='runs the "lights" Raspberry Pi')
 
-    args = parser.parse_args()
+	args = parser.parse_args()
 
-    if args.piPlatform == 'lights':
-        runLights()
-    if args.piPlatform == 'door':
-        runDoor()
+	if args.piPlatform == 'lights':
+		runLights()
+	if args.piPlatform == 'door':
+		runDoor()
